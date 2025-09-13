80cc83927b5f2510d263026ee14fc7034b0f51be
Prathamesh <80304812+PageNotFound222@users.noreply.github.com>
2025-09-13 13:59:24 +0530

    Create call by value.cpp

---
+++ call by value.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void swap(int x,int y){
+    int temp;
+    temp=x;
+    x=y;
+    y=temp;
+}
+int main() {
+    int a=20,b=50;
+    swap(a,b);
+    cout<<a<<endl<<b;
+}
+/*
+output:
+20
+50
+*/
